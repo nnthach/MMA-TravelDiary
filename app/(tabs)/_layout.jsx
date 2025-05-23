a9538e8e380fe268345da1b6e1ae11833267c740
nnthach <nguyenngocthach2301@gmail.com>
2025-05-23 09:07:14 +0700

    Home screen and bookcard component

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,20 +1,69 @@
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerTitleAlign: "center",
+        tabBarShowLabel: false,
         tabBarInactiveTintColor: "black",
+        tabBarActiveTintColor: "black",
         tabBarStyle: {
-          height: 50,
+          height: 40,
           borderTopWidth: 0,
         },
       }}
     >
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="books" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="books"
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "book" : "book-outline"}
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "search-circle" : "search-circle-outline"}
+              size={28}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
